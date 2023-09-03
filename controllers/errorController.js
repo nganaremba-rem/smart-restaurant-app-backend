4af04647c78b9c60bd32a771bf6f2a11144ad44f
Mandar <mandardeshpande2003@gmail.com>
2023-09-03 19:03:03 +0530

    bug fixes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,8 @@
 module.exports = (error, req, res, next) => {
   error.status = error.status || "error";
+  if (!error.statusCode) {
+    error.statusCode = 500;
+  }
   res.status(error.statusCode).json({
     status: error.statusCode,
     message: error.message,
