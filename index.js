1a64e5a21fc2b74683ecbb113efab141596e4c62
Mandar <mandardeshpande2003@gmail.com>
2023-09-29 22:27:42 +0530

    solves security issues

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
-  
+
 const DB_URL = process.env.DB_URL;
 
-mongoose.connect("mongodb+srv://kalidindidhrutika:1234KALIDINDI@cluster0.jerf3wp.mongodb.net/").then(() => console.log("DB connection successful!"));
+mongoose.connect(DB_URL).then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
