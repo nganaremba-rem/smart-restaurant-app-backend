b4374d80dc175cfe9b1fe7d3116fe81b31770dd8
Nganaremba <nganaremba-rem>
2024-04-26 12:17:28 +0530

    some minor changes

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -6,60 +6,58 @@ const jwt = require("jsonwebtoken");
 const CustomError = require("../customError");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "10d",
-  });
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: "10d",
+	});
 };
 
 exports.sendOtp = asyncHandler(async (email) => {
-  const min = 100000;
-  const max = 999999;
-  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  randomNumber = randomNumber.toString();
-  const message =
-    randomNumber.toString() +
-    " is the OTP to login to you Smart Restaurant App account. Do not disclose it to anyone.";
-  const mailer = new Mailer(email, message, "Smart Restaurant App");
-  mailer
-    .sendEmail()
-    .then(() => {
-      return true;
-    })
-    .catch((err) => {
-      return false;
-    });
-  await OTP.create({
-    email,
-    code: randomNumber,
-    createdAt: new Date(),
-  });
+	const min = 100000;
+	const max = 999999;
+	let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+	randomNumber = randomNumber.toString();
+	const message = `${randomNumber.toString()} is the OTP to login to you Smart Restaurant App account. Do not disclose it to anyone.`;
+	const mailer = new Mailer(email, message, "Smart Restaurant App");
+	mailer
+		.sendEmail()
+		.then(() => {
+			return true;
+		})
+		.catch((err) => {
+			return false;
+		});
+	await OTP.create({
+		email,
+		code: randomNumber,
+		createdAt: new Date(),
+	});
 });
 
 exports.verifyOtp = asyncHandler(async (req, res) => {
-  const { email, enteredOTP } = req.body;
-  if (!email || email.length === 0 || !enteredOTP === 0) {
-    throw new CustomError("Please enter OTP and email", 400);
-  }
-  const arr = await OTP.find({ email }).sort({ createdAt: -1 });
-  const storedOtp = arr[0];
-  if (!storedOtp) {
-    throw new CustomError("Something went wrong");
-  }
-  if (storedOtp.code == enteredOTP) {
-    const newUser = await User.findOneAndUpdate(
-      { email },
-      { isVerified: true }
-    );
-    res.status(201).json({
-      _id: newUser._id,
-      firstName: newUser.firstName,
-      lastName: newUser.lastName,
-      role: newUser.role,
-      email: newUser.email,
-      isVerified: newUser.isVerified,
-      token: generateToken(newUser._id),
-    });
-  } else {
-    throw new CustomError("incorrect OTP", 400);
-  }
+	const { email, enteredOTP } = req.body;
+	if (!email || email.length === 0 || !enteredOTP === 0) {
+		throw new CustomError("Please enter OTP and email", 400);
+	}
+	const arr = await OTP.find({ email }).sort({ createdAt: -1 });
+	const storedOtp = arr[0];
+	if (!storedOtp) {
+		throw new CustomError("Something went wrong");
+	}
+	if (storedOtp.code == enteredOTP) {
+		const newUser = await User.findOneAndUpdate(
+			{ email },
+			{ isVerified: true },
+		);
+		res.status(201).json({
+			_id: newUser._id,
+			firstName: newUser.firstName,
+			lastName: newUser.lastName,
+			role: newUser.role,
+			email: newUser.email,
+			isVerified: newUser.isVerified,
+			token: generateToken(newUser._id),
+		});
+	} else {
+		throw new CustomError("incorrect OTP", 400);
+	}
 });
--- index.js
+++ index.js
@@ -13,117 +13,117 @@ mongoose.connect(DB_URL).then(() => console.log("DB connection successful!"));
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  //done
-  socket.on("join_waiters_room", (data) => {
-    const { waiter } = data;
-    const rooms = Array.from(socket.rooms); // Get all rooms the socket has joined
-    // console.log(rooms);
+	//done
+	socket.on("join_waiters_room", (data) => {
+		const { waiter } = data;
+		const rooms = Array.from(socket.rooms); // Get all rooms the socket has joined
+		// console.log(rooms);
 
-    if (!rooms.includes(waiter) && !rooms.includes("waiters_room")) {
-      socket.join(waiter);
-      socket.join("waiters_room");
-      // console.log("joined_waiters_room ", waiter, socket.rooms);
-    } else {
-      // console.log("waiter Socket is already a member of this room");
-    }
-  });
+		if (!rooms.includes(waiter) && !rooms.includes("waiters_room")) {
+			socket.join(waiter);
+			socket.join("waiters_room");
+			// console.log("joined_waiters_room ", waiter, socket.rooms);
+		} else {
+			// console.log("waiter Socket is already a member of this room");
+		}
+	});
 
-  //done
-  socket.on("join_chefs_room", (data) => {
-    const rooms = Array.from(socket.rooms); // Get all rooms the socket has joined
-    // console.log(rooms);
-    const { chef } = data;
-    if (!rooms.includes(chef) && !rooms.includes("chefs_room")) {
-      socket.join(`${chef}`);
-      socket.join("chefs_room");
-      // console.log("chef joined room: " + chef, socket.rooms);
-    } else {
-      // console.log("chef Socket is already a member of this room");
-    }
-  });
+	//done
+	socket.on("join_chefs_room", (data) => {
+		const rooms = Array.from(socket.rooms); // Get all rooms the socket has joined
+		// console.log(rooms);
+		const { chef } = data;
+		if (!rooms.includes(chef) && !rooms.includes("chefs_room")) {
+			socket.join(`${chef}`);
+			socket.join("chefs_room");
+			// console.log("chef joined room: " + chef, socket.rooms);
+		} else {
+			// console.log("chef Socket is already a member of this room");
+		}
+	});
 
-  // done
-  socket.on("join_customer_room", (data) => {
-    const rooms = Array.from(socket.rooms); // Get all rooms the socket has joined
-    // console.log(rooms);
-    const { customer } = data;
-    if (!rooms.includes(customer)) {
-      socket.join(`${customer}`);
-      // console.log("customer joined room: " + customer, socket.rooms);
-    } else {
-      // console.log("customer Socket is already a member of this room");
-    }
-  });
+	// done
+	socket.on("join_customer_room", (data) => {
+		const rooms = Array.from(socket.rooms); // Get all rooms the socket has joined
+		// console.log(rooms);
+		const { customer } = data;
+		if (!rooms.includes(customer)) {
+			socket.join(`${customer}`);
+			// console.log("customer joined room: " + customer, socket.rooms);
+		} else {
+			// console.log("customer Socket is already a member of this room");
+		}
+	});
 
-  // done
-  socket.on("order_placed", (data) => {
-    const { tableNumber } = data;
-    socket
-      .to("waiters_room")
-      .emit("pick_order", `order placed from table no. ${tableNumber}`);
-    // console.log("Order placed", tableNumber);
-    notificationController.createNotification({
-      message: `order placed from table no. ${tableNumber}`,
-      group: "waiters",
-    });
-  });
+	// done
+	socket.on("order_placed", (data) => {
+		const { tableNumber } = data;
+		socket
+			.to("waiters_room")
+			.emit("pick_order", `order placed from table no. ${tableNumber}`);
+		// console.log("Order placed", tableNumber);
+		notificationController.createNotification({
+			message: `order placed from table no. ${tableNumber}`,
+			group: "waiters",
+		});
+	});
 
-  //done
-  socket.on("order_confirmed", (data) => {
-    const { customer, tableNumber } = data;
-    socket.to(`${customer}`).emit("waiter_confirmed", `You order is confirmed`);
-    socket
-      .to("chefs_room")
-      .emit(
-        "waiter_confirmed",
-        `Start preparation for table number ${tableNumber}`
-      );
-    notificationController.createNotification({
-      receiver: customer,
-      message: `You order is confirmed`,
-    });
-    notificationController.createNotification({
-      message: `Start preparation for table number ${tableNumber}`,
-      group: "chefs",
-    });
+	//done
+	socket.on("order_confirmed", (data) => {
+		const { customer, tableNumber } = data;
+		socket.to(`${customer}`).emit("waiter_confirmed", "You order is confirmed");
+		socket
+			.to("chefs_room")
+			.emit(
+				"waiter_confirmed",
+				`Start preparation for table number ${tableNumber}`,
+			);
+		notificationController.createNotification({
+			receiver: customer,
+			message: "You order is confirmed",
+		});
+		notificationController.createNotification({
+			message: `Start preparation for table number ${tableNumber}`,
+			group: "chefs",
+		});
 
-    // console.log("order confirmed notification sent to ", customer, tableNumber);
-  });
+		// console.log("order confirmed notification sent to ", customer, tableNumber);
+	});
 
-  //done
-  socket.on("preparation_started", (data) => {
-    const { customer } = data;
-    socket
-      .to(`${customer}`)
-      .emit("chef_started", "Chef started preparing your order");
-    notificationController.createNotification({
-      receiver: customer,
-      message: `Chef started preparing your order`,
-    });
+	//done
+	socket.on("preparation_started", (data) => {
+		const { customer } = data;
+		socket
+			.to(`${customer}`)
+			.emit("chef_started", "Chef started preparing your order");
+		notificationController.createNotification({
+			receiver: customer,
+			message: "Chef started preparing your order",
+		});
 
-    // console.log("preparation started", customer);
-  });
+		// console.log("preparation started", customer);
+	});
 
-  // done
-  socket.on("order_is_ready", (data) => {
-    const { customer, waiter, tableNumber } = data;
-    socket.to(`${customer}`).emit("chef_ended", "your order is ready");
-    socket
-      .to(`${waiter}`)
-      .emit("chef_ended", `Order from table ${tableNumber} is ready`);
-    notificationController.createNotification({
-      receiver: customer,
-      message: `your order is ready`,
-    });
-    notificationController.createNotification({
-      receiver: waiter,
-      message: `Order from table ${tableNumber} is ready`,
-    });
-    // console.log("order ready ", waiter, customer);
-  });
+	// done
+	socket.on("order_is_ready", (data) => {
+		const { customer, waiter, tableNumber } = data;
+		socket.to(`${customer}`).emit("chef_ended", "your order is ready");
+		socket
+			.to(`${waiter}`)
+			.emit("chef_ended", `Order from table ${tableNumber} is ready`);
+		notificationController.createNotification({
+			receiver: customer,
+			message: "your order is ready",
+		});
+		notificationController.createNotification({
+			receiver: waiter,
+			message: `Order from table ${tableNumber} is ready`,
+		});
+		// console.log("order ready ", waiter, customer);
+	});
 });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  // console.log(`App running on port ${port}...`);
+	console.log(`App running on port ${port}...`);
 });
